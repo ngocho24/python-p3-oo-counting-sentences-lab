0dca7ecbe5c00a32f4928966e7741d85e9ecff34
Elijah Ngocho <elijahngocho24@gmail.com>
2023-12-01 09:39:06 +0300

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            raise ValueError("MyString must be initialized with a string value.")
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+     if not isinstance(new_value, str):
+        raise ValueError("MyString must have a string value.")
+     self._value = new_value
+
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        # Split the value using period, question mark, and exclamation mark as delimiters
+        sentences = [sentence.strip() for sentence in re.split(r'[.!?]', self._value) if sentence]
+        return len(sentences)
+       
